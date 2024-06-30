e418aade4fcac08ca3cbd4ba6a182bb1c10cac16
jaydipjadhav <jaydipdjadhav>
2024-06-30 16:01:40 +0530

    fetch all todos using .then

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+
+interface Itodo{
+  userId : number;
+  id : number;
+  title : string;
+  completed :boolean
+}
+   
+      
+
+
 function App() {
+  const[data , setData] =useState<Itodo[]>([])
+
+  useEffect(() =>{
+    //using then
+    fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((response) => response)
+    .then((e) => e.json())
+    .then((e) => {
+      console.log(e)
+      return setData(e as Itodo[])
+
+     
+
+    })
+
+  },[])
+
+  console.log("data : " , data)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {
+                 data.map(todo  => <div key={todo.id}>
+                                        <p>{todo.userId}</p> 
+                                        <li >{todo.title}</li>
+                                    </div>                       
+                
+                 )              
+
+        }
+        
       </header>
     </div>
   );
